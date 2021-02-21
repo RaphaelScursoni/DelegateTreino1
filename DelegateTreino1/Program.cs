b9fceb42e0543a67fad1d13347ebe7bb923cd0e2
Raphael Scursoni <ra.scursoni@gmail.com>
2021-02-21 09:16:20 -0300

    Adicionando delegate em uma operacao

---
+++ DelegateTreino1/Program.cs
@@ -0,0 +1,22 @@
+﻿using DelegateTreino1.Services;
+using System;
+
+namespace DelegateTreino1 {
+
+    delegate double BinaryNumericOpetation(double n1, double n2);
+    
+    class Program {
+        static void Main(string[] args) {
+
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOpetation op = CalculationService.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
---
+++ DelegateTreino1/Services/CalculationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateTreino1.Services {
+    class CalculationService {
+        public static double Max(double x, double y) {
+            return (x > y) ? x : y;
+        } // MAIOR
+        public static double Sum(double x, double y) {
+            return x + y;
+        } // SOMA
+        public static double Square(double x) {
+            return x * x;
+        } // QUADRADO 
+    }
+}
